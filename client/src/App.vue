390a2455ff138729da29068b00403f901b2918f5
johandlalicon <johandlalicon@hotmail.com>
2023-09-02 02:44:25 +0800

    add cloudinary widget

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,28 @@
 <template>
   <Navbar />
-  <div class="mt-10">
+  <div class="m-10">
     <router-view></router-view>
   </div>
+  <div>
+    <button @click="openUploadWidget">Upload</button>
+  </div>
 </template>
+
 <script setup>
 import Navbar from './components/Navbar.vue';
+
+const widget = cloudinary.createUploadWidget(
+  { cloud_name: "dgks1y9qz", upload_preset: "waejp7di" },
+  (err, result) => {
+    if (!err && result && result.event === "success")
+      console.log("Uploading done", result.info)
+  }
+)
+
+function openUploadWidget() {
+  widget.open();
+}
+
 </script>
 
 <style scoped></style>
