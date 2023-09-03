8fa9025a02487e7b051279e0ade125575dd0d2f8
johandlalicon <johandlalicon@hotmail.com>
2023-09-03 13:22:46 +0800

    update front

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -21,24 +21,56 @@ const logout = () => {
 
 </script>
 <template>
-    <nav class="bg-gray-200 p-4 shadow-lg gap-8">
-        <div class="flex justify-between items-center">
+    <nav class="bg-gray-800">
+        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div class="relative flex h-16 items-center justify-between">
+                <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
 
-            <div class="flex gap-4">
-                <a href="#">Home</a>
-                <a href="#">Services</a>
+                    <div class="flex space-x-4">
+                        <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                        <a href="#" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+                            aria-current="page">Dashboard</a>
+                        <a href="#"
+                            class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Services</a>
 
-                <router-link v-if="type === 'Merchant'" to="/merchant">Merchant</router-link>
+                        <router-link v-if="type === 'Merchant'" to="/merchant"
+                            class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Merchant</router-link>
+                    </div>
+
+                </div>
+                <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+
+                    <div v-if="!isLoggedIn" class="justify-self-end">
+                        <Login />
+                    </div>
+                    <div v-else class="flex gap-4">
+                        <button @click="logout" class="py-2 px-4 text-sm bg-sky-500 text-white">Logout</button>
+                    </div>
+                </div>
             </div>
-            <div v-if="!isLoggedIn" class="justify-self-end">
-                <Login />
-            </div>
-            <div v-else class="flex gap-4">
-                <span>{{ type }}</span>
-                <button @click="logout">Logout</button>
-
-            </div>
-
         </div>
+
+        <!-- Mobile menu, show/hide based on menu state. -->
+
     </nav>
 </template>
+<!-- <nav class="bg-gray-200">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+
+        <div class="flex gap-4">
+            <a href="#">Home</a>
+            <a href="#">Services</a>
+
+            <router-link v-if="type === 'Merchant'" to="/merchant">Merchant</router-link>
+        </div>
+        <div v-if="!isLoggedIn" class="justify-self-end">
+            <Login />
+        </div>
+        <div v-else class="flex gap-4">
+            <span>{{ type }}</span>
+            <button @click="logout">Logout</button>
+
+        </div>
+
+    </div>
+</nav> -->
--- client/src/views/MerchantPage.vue
+++ client/src/views/MerchantPage.vue
@@ -1,4 +1,27 @@
 <template>
     <h1>MERCHANT DASHBOARD</h1>
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div class="relative flex h-16 items-center justify-between">
+            <div class="flex flex-1 items-center justify-center ">
+                <div class="flex space-x-4">
+                    <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                    <a href="#"
+                        class="text-gray-600 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">All
+                        Services</a>
+                    <a href="#"
+                        class="text-gray-600 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">All
+                        Category</a>
+
+                    <router-link v-if="type === 'Merchant'" to="/merchant"
+                        class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Merchant</router-link>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <ServiceCard />
 </template>
-<script setup></script>
+<script setup>
+import ServiceCard from '../components/ServiceCard.vue';
+</script>
