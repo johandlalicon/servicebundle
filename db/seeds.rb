b42e8e1e83df9be7ea0ab696d5a5e24577b2ede7
johandlalicon <johandlalicon@hotmail.com>
2023-09-07 16:33:52 +0800

    update seed due to error

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,11 +18,12 @@ Merchant.create!(
 )
 
 User.create!(
-  first_name: "Admin"
-  last_name: "Admin"
-  mobile_number: "12345678"
-  email: "admin@admin.com"
-  password: "123456"
+  first_name: "Admin",
+  last_name: "Admin",
+  mobile_number: "12345678",
+  email: "admin@admin.com",
+  password: "123456",
+  password_confirmation: "123456",
   isAdmin: true
 )
   
