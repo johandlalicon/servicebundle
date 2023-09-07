204625f1037e38acfd3dd7b8fe55d2b397b731af
johandlalicon <johandlalicon@hotmail.com>
2023-09-07 16:59:48 +0800

    disable seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,29 @@
-# db/seeds.rb
+# # db/seeds.rb
 
 
-# Sample merchants
-Merchant.create!(
-  name: "Cleaning Company",
-  email: "test1@merchant.com",
-  password: "123456",
-  password_confirmation: "123456"
-)
+# # Sample merchants
+# Merchant.create!(
+#   name: "Cleaning Company",
+#   email: "test1@merchant.com",
+#   password: "123456",
+#   password_confirmation: "123456"
+# )
 
   
-Merchant.create!(
-    name: "Pet Grooming Company",
-    email: "test2@merchant.com",
-    password: "123456",
-    password_confirmation: "123456"
-)
+# Merchant.create!(
+#     name: "Pet Grooming Company",
+#     email: "test2@merchant.com",
+#     password: "123456",
+#     password_confirmation: "123456"
+# )
 
-User.create!(
-  first_name: "Admin",
-  last_name: "Admin",
-  mobile_number: "12345678",
-  email: "admin@admin.com",
-  password: "123456",
-  password_confirmation: "123456",
-  isAdmin: true
-)
+# User.create!(
+#   first_name: "Admin",
+#   last_name: "Admin",
+#   mobile_number: "12345678",
+#   email: "admin@admin.com",
+#   password: "123456",
+#   password_confirmation: "123456",
+#   isAdmin: true
+# )
   
