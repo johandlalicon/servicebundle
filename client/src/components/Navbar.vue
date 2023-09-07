5b56a0bf3a98aead17619a77c6cab6c4a45e84d4
johandlalicon <johandlalicon@hotmail.com>
2023-09-07 19:42:26 +0800

    update navbar

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -32,7 +32,7 @@ const logout = () => {
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                         <a href="#" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
                             aria-current="page">Dashboard</a>
-                        <router-link v-if="type === 'User'" to="/marketplace"
+                        <router-link v-if="type !== 'Admin' && type !== 'Merchant'" to="/marketplace"
                             class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Marketplace</router-link>
 
 
